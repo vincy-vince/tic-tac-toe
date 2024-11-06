d045f7ef395505d7f6bc102dd1f96595dd8a023a
vincy <vincyvince510@gmail.com>
2024-11-06 13:05:03 +0530

    finished first 2 problem statement

--- ttt.js
+++ ttt.js
@@ -12,37 +12,49 @@ const winningMove = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-let latestMove;
-console.log(latestMove);
+
+let latestMove = "";
 
 cells.forEach((cell) => {
   cell.addEventListener("click", (event) => {
     const index = event.target.dataset.index;
     let player = event.target;
-    playerMove(latestMove);
-    event.target.innerText = playerMove(latestMove);
-    // nextMove(latestMove);
+
+    function move() {
+      if (latestMove === "" || latestMove === "O") {
+        console.log("X");
+        latestMove = "X";
+      } else {
+        console.log("O");
+       latestMove = "O";
+      }
+    }
+    move();
+    
+    if(event.target.innerText === ""){
+      event.target.innerText = latestMove
+    }else{
+      alert('already filled');
+      move(); 
+    }
 
   });
 });
 
-function playerMove(latestMove) {
-  console.log("playermove");
-  // latestMove === undefined
-  latestMove = "X";
-  console.log("x");
-  return latestMove;
-}
+// function playerMove(latestMove) {
+//   latestMove = "X";
+//   return latestMove;
+// }
 
-function nextMove(latestMove) {
-  let move = "";
-  if (latestMove === "X") {
-    latestMove = "O";
-    console.log("O");
-  } else {
-    latestMove = "X";
-  }
-}
+// function nextMove(latestMove) {
+//   let move = "";
+//   if (latestMove === "X") {
+//     latestMove = "O";
+//     console.log("O");
+//   } else {
+//     latestMove = "X";
+//   }
+// }
 function avoidRepetition() {}
 
 function restart() {
@@ -56,3 +68,7 @@ function playNow() {
 function winner() {
   // to predict win
 }
+
+// problem statements:
+// 1. Alternative players for each onclick events.
+// 2. Alert when clicking in the filled box.
