07348317327cde2495ea72f2ed972725c58d0d7a
vincy <vincyvince510@gmail.com>
2024-11-06 17:22:23 +0530

    new problem unlocked

--- ttt.js
+++ ttt.js
@@ -1,23 +1,21 @@
 const playnow = document.querySelector(".play-now");
-const reStart = document.querySelector(".restart");
+
 const cells = document.querySelectorAll(".cell");
-
-const winningMove = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
 let latestMove = "X";
+let xIndex = [];
+let oIndex = [];
 
 cells.forEach((cell) => {
   cell.addEventListener("click", (event) => {
     const index = event.target.dataset.index;
+    if (latestMove === "X") {
+      xIndex.push(index);
+    } else if (latestMove === "O") {
+      oIndex.push(index);
+    }
+    console.log(xIndex);
+    console.log(oIndex);
+
     let player = event.target;
 
     if (event.target.innerText === "") {
@@ -29,43 +27,65 @@ cells.forEach((cell) => {
     }
     function move() {
       if (latestMove === "O") {
-        console.log("X");
         latestMove = "X";
       } else {
-        console.log("O");
         latestMove = "O";
       }
     }
+    // winner();
+    winner();
   });
 });
-
-// function playerMove(latestMove) {
-//   latestMove = "X";
-//   return latestMove;
-// }
-
-// function nextMove(latestMove) {
-//   let move = "";
-//   if (latestMove === "X") {
-//     latestMove = "O";
-//     console.log("O");
-//   } else {
-//     latestMove = "X";
-//   }
-// }
-function avoidRepetition() {}
-
-function restart() {
-  // to clear all the moves and restart
+function winner() {
+  const winningMove = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  
+  winningMove.forEach((val,i) => {
+  val.forEach((value,i) => {
+     if(val[i] == xIndex){
+      console.log('equal');
+     }
+  });
+  });
 }
+
+// function restart() {
+// const reStart = document.querySelector(".restart");
+
+// let indexEle;
+
+//   const cellEle = document.querySelectorAll(".cell");
+//   cellEle.forEach((cell) => {
+//     cell.addEventListener("click", (event) => {
+//       indexEle = event.target.dataset.index;
+//       console.log(indexEle);
+//     });
+//   });
+
+//   winningMove.forEach((value) => {
+//     value.forEach((arr) => {
+//       let arrIndex = arr;
+//       console.log(arrIndex);
+//     });
+//   });
+
+// to clear all the moves and restart
+// }
 
 function playNow() {
   // start the game
 }
 
-function winner() {
-  // to predict win
-}
+// if index of x = value // x wins,then breaks.
+// if index of O = value // O wins,then breaks.
 
 // problem statements:
 // 1. Alternative players for each onclick events.
