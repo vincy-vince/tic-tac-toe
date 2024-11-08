a4d4e57b6d1a27df000ceada90684685dfb28e2c
vincy <vincyvince510@gmail.com>
2024-11-08 15:11:14 +0530

    old

---
+++ practice.js
@@ -0,0 +1,15 @@
+const winningMove = [
+  [0, 3, 6],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+for (let i = 0; i < winningMove.length; i++) {
+  const element = winningMove[i];
+  console.log(element);
+}
